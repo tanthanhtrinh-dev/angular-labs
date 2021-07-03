e234797d945cf55bb06e7a63511d26d20a42ff71
tanthanhtrinh-dev <tanthanhtrinh.dev@gmail.com>
2021-07-03 17:04:18 +0700

    formControl's done

--- ReactiveFroms/src/app/app-routing.module.ts
+++ ReactiveFroms/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AccountComponent } from './accounts/account/account.component';
+import { CustomerComponent } from './customers/customer.component';
+
+const routes: Routes = [
+  { path: 'customer', component: CustomerComponent },
+  { path: 'account', component: AccountComponent },
+  { path: '', redirectTo: 'customer', pathMatch: 'full' },
+  { path: '**', redirectTo: 'customer', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ReactiveFroms/src/app/app.module.ts
+++ ReactiveFroms/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
+import { AccountComponent } from './accounts/account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
