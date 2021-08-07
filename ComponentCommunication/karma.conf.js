5b8521c4137532e2cfb3d59f53e85087611e380e
tanthanhtrinh-dev <tanthanhtrinh.dev@gmail.com>
2021-08-07 09:01:59 +0700

    add app to learn component communication

--- ComponentCommunication/karma.conf.js
+++ ComponentCommunication/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/apm'),
+      dir: require('path').join(__dirname, './coverage/ComponentCommunication'),
       subdir: '.',
       reporters: [
         { type: 'html' },
